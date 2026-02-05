8971227e5b640882905c4aa26fae90c733ab675d
Ninazu00 <ba.mohammad2005@gmail.com>
2026-02-05 16:00:16 +0200

    Fixing vite.config.js to support older Node JS

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,11 @@
+import { randomFillSync } from 'node:crypto';
+// This shims the missing crypto method before Vite starts
+if (typeof globalThis.crypto === 'undefined') {
+  globalThis.crypto = {
+    getRandomValues: (buffer) => randomFillSync(buffer)
+  };
+}
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
